b856f4bebcfa856ea507e381d632472fe3add4c1
Aymen Kabba <Aymenkb98@gmail.com>
2024-01-31 19:00:13 +0000

    first commit

---
+++ src/main/java/ma/aymen/springbootkafkademoapp/SpringbootKafkaDemoAppApplication.java
@@ -0,0 +1,14 @@
+package ma.aymen.springbootkafkademoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringbootKafkaDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaDemoAppApplication.class, args);
+    }
+
+}
